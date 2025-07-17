d8021afaf7678421bd1e7e100fbd493356d87556
Javelinxd <104097419+siabas@users.noreply.github.com>
2025-07-17 20:30:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ module.exports = {
         status: "Abone Botu Altyapısı"
     },
     Settings: {
-        abonerolü: "1395075974495998062",
-        aboneyetkilisi: "1395075992653136042",
-        abonelog: "1395455532609044684"
+        abonerolü: "",
+        aboneyetkilisi: "",
+        abonelog: ""
     },
     Emojis: {
         cross: "❌",
